a85ac7589d94e1d5233611b54bb28a4025bdd7a4
Janette Day <janette.day@civicactions.com>
2018-04-20 16:53:02 -0500

    Update colorizer and add _site_information_theme_settings_form_submit function

--- template.php
+++ template.php
@@ -4,6 +4,17 @@
  * Theme functions
  */
 
+// require_once dirname(__FILE__) . '/includes/block.inc';
+// require_once dirname(__FILE__) . '/includes/comment.inc';
+// require_once dirname(__FILE__) . '/includes/form.inc';
+// require_once dirname(__FILE__) . '/includes/menu.inc';
+// require_once dirname(__FILE__) . '/includes/node.inc';
+// require_once dirname(__FILE__) . '/includes/panel.inc';
+// require_once dirname(__FILE__) . '/includes/search.inc';
+// require_once dirname(__FILE__) . '/includes/structure.inc';
+// require_once dirname(__FILE__) . '/includes/user.inc';
+// require_once dirname(__FILE__) . '/includes/view.inc';
+
 // Include all files from the includes directory.
 $includes_path = dirname(__FILE__) . '/includes/*.inc';
 foreach (glob($includes_path) as $filename) {
@@ -16,7 +27,7 @@ foreach (glob($includes_path) as $filename) {
 function {{machine_name}}_preprocess_page(&$variables) {
   // Add copyright to theme.
   if ($copyright = theme_get_setting('copyright')) {
-    $variables['copyright'] = isset($copyright['value']) ? check_markup($copyright['value'], $copyright['format']) : t('Powered by <a href="http://nucivic.com/dkan">DKAN</a>, a project of <a href="http://nucivic.com">NuCivic</a>'); 
+    $variables['copyright'] = isset($copyright['value']) ? check_markup($copyright['value'], $copyright['format']) : t('Powered by <a href="http://nucivic.com/dkan">DKAN</a>, a project of <a href="http://nucivic.com">NuCivic</a>');
   }
   $variables['display_login_menu'] = (theme_get_setting('display_login_menu') === NULL) ? 1 : theme_get_setting('display_login_menu');
 }
@@ -31,4 +42,13 @@ function {{machine_name}}_form_alter(&$form, &$form_state, $form_id) {
       $form['colorizer_global']['colorizer_incfile']['#default_value'] = 'colorizer/colorizer.inc';
       break;
   }
+}
+
+/**
+ * Submit function for theme settings form information.
+ */
+function {{machine_name}}_site_information_theme_settings_form_submit(&$form, &$form_state) {
+  variable_set('site_name', $form_state['values']['site_name']);
+  variable_set('site_slogan', $form_state['values']['site_slogan']);
+  variable_set('site_mail', $form_state['values']['site_mail']);
 }
